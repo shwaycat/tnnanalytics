3f00d7ddb3eea692d3782a91a38177033aef720b
stonean <drew@stonean.com>
2015-03-17 16:38:18 +0000

    remove test code from route

--- routes/views/site/me.js
+++ routes/views/site/me.js
@@ -1,7 +1,6 @@
 var keystone = require('keystone')
 	, _ = require('underscore')
 	, Twitter = require('twitter')
-	, FB = require('fb')
 
 exports = module.exports = function(req, res) {
 
@@ -10,69 +9,6 @@ exports = module.exports = function(req, res) {
 
 	locals.section = 'me';
 	locals.page.title = locals.site.brand + ' Settings';
-	locals.data = {
-		twitter: []
-	}
-
-
-	/*/
-		Load Facebook data
-
-	 	Nookwit page id 464286813678947
-	/*/
-	view.on('init', function(next) {
-		return next()
-		FB.setAccessToken(req.user.services.facebook.accessToken)
-
-		FB.api('TeamNovoNordisk/feed',function (results) {
-			if(!results || results.error) {
-				console.log(!results ? 'error occurred' : results.error);
-				return;
-			}
-			_.each(results,function(result){
-				//console.log('1111111111111111111111111111111111111111111')
-				_.each(result, function(res) {
-					//console.log('2222222222222222222222222222222222222222222')
-					//console.log(res)
-					FB.api(res.id+'/comments',function (comments) {
-						console.log('3333333333333333333333333333333333333333333')
-						console.log(comments)
-						_.each(comments.data, function(comment){
-							console.log('4444444444444444444444444444444444444444444')
-							console.log(comment.from)
-							FB.api(comment.from.id,function (profile) {
-								console.log('5555555555555555555555555555555555555555555')
-								console.log(profile)
-							})
-						})
-					})
-				})
-			})
-			next()
-		});
-	})
-
-	// Load Twitter data
-	view.on('init', function(next) {
-	//skip for now
-		return next()
-		var client = new Twitter({
-			consumer_key: process.env.TWITTER_API_KEY,
-			consumer_secret: process.env.TWITTER_API_SECRET,
-			access_token_key: req.user.services.twitter.accessToken,
-			access_token_secret: req.user.services.twitter.refreshToken,
-		})
-
-		client.get('statuses/user_timeline', {}, function(err, tweets, response){
-				if (err) {
-					console.log(err)
-					next(err)
-				}
-				console.log(tweets)
-				locals.data.twitter = tweets
-				next()
-		});
-	})
 
 	/*/
 		Handle disconnect requests
