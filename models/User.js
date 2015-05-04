936243284816541061b5c17f29b0a6e00927d7e8
Travis Warlick <twarlick@maxmedia.com>
2015-05-04 17:32:32 -0400

    remove unnecessary junk from user object, cleanup User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,8 @@
-var keystone = require('keystone')
-  , async = require('async')
-  , crypto = require('crypto')
-  , _ = require('underscore')
-  , request = require('request')
-  , Types = keystone.Field.Types
+var keystone = require('keystone'),
+    crypto = require('crypto'),
+    _ = require('underscore'),
+    request = require('request'),
+    Types = keystone.Field.Types;
 
 /**
  * User model
@@ -14,13 +13,8 @@ var keystone = require('keystone')
  * @member {String} resetPasswordKey - TODO
  * @member {String} accountName - account that the user belongs to
  * @member {Boolean} isAccountRoot - indicates if the user is the top-level/root/main user of the account
- * @member {Boolean} isPublic - Profile - indicates if the profile is public
- * @member {String} twitter - Profile - TODO
- * @member {Field~Types~Url} website - Profile - TODO
- * @member {String} gravatar - Profile - TODO
  * @member {Field~Types~Textarea} notifications.keywords - Notifications - comma separated list of keywords/keyphrases for alerting
  * @member {Boolean} isAdmin - Permissions - TODO
- * @member {Boolean} isVerified - Permissions - TODO
  * @member {UserServices} services - hashtable of services by key
  */
 
@@ -33,7 +27,7 @@ var deps = {
   facebook: { 'services.facebook.isConfigured': true },
   google: { 'services.google.isConfigured': true },
   twitter: { 'services.twitter.isConfigured': true }
-}
+};
 
 User.add({
   name: { type: Types.Name, required: true, index: true },
@@ -41,20 +35,14 @@ User.add({
   password: { type: Types.Password, initial: true },
   resetPasswordKey: { type: String, hidden: true },
   accountName: { type: String, required: true, index: true, initial: true },
-  isAccountRoot: { type: Boolean, default: false }
-}, 'Profile', {
-  isPublic: { type: Boolean, default: true },
-  twitter: { type: String, width: 'short' },
-  website: { type: Types.Url },
-  gravatar: { type: String, noedit: true }
+  isAccountRoot: { type: Boolean, "default": false }
 }, 'Notifications', {
-//TODO make keywords top-level, not nested
+  //TODO make keywords top-level, not nested
   notifications: {
-    keywords: { type: Types.Textarea, label: 'Keywords'},
+    keywords: { type: Types.Textarea, label: 'Keywords'}
   }
 }, 'Permissions', {
-  isAdmin: { type: Boolean, label: 'Can Admin ' + keystone.get('brand') },
-  isVerified: { type: Boolean, label: 'Has a verified email address' }
+  isAdmin: { type: Boolean, label: 'Can Admin ' + keystone.get('brand') }
 }, 'Services', {
   /**
    * User Services object - hashtable of services by key
@@ -69,51 +57,50 @@ User.add({
     /**
      * Facebook Service
      * @typedef {Object} UserServices~Facebook
-     * @member {Boolean} isConfigured -
-     * @member {String} profileId -
-     * @member {String} username -
-     * @member {String} avatar -
-     * @member {String} accessToken -
-     * @member {String} refreshToken -
-     * @member {String} lastPostTime -
-     * @member {String} lastMessageTime -
+     * @member {Boolean} isConfigured - `true` if configured
+     * @member {String} profileId - Facebook user profile ID
+     * @member {String} username - Facebook username
+     * @member {String} accessToken - OAuth access token
+     * @member {String} refreshToken - OAuth refresh token
+     * @member {String} pageID - Facebook page ID
     */
     facebook: {
       isConfigured: { type: Boolean, label: 'Facebook has been authenticated' },
-
       profileId: { type: String, label: 'Profile ID', dependsOn: deps.facebook },
-
       username: { type: String, label: 'Username', dependsOn: deps.facebook },
-      avatar: { type: String, label: 'Image', dependsOn: deps.facebook },
-
       accessToken: { type: String, label: 'Access Token', dependsOn: deps.facebook },
       refreshToken: { type: String, label: 'Refresh Token', dependsOn: deps.facebook },
-
-      lastPostTime: { type: String, label: 'Last Post Time', dependsOn: deps.facebook },
-      lastMessageTime: { type: String, label: 'Last Message Time', dependsOn: deps.facebook}
-        //pageIds: { type: [String], label: 'Page Ids', dependsOn: deps.pages }
-
-
+      pageID: { type: String, label: 'Page ID', dependsOn: deps.pages }
     },
+    /**
+     * Google Service
+     * @typedef {Object} UserServices~Google
+     * @member {Boolean} isConfigured - `true` if configured
+     * @member {String} profileId - Google user profile ID
+     * @member {String} username - Google username
+     * @member {String} accessToken - OAuth access token
+     * @member {String} refreshToken - OAuth refresh token
+    */
     google: {
       isConfigured: { type: Boolean, label: 'Google has been authenticated' },
-
       profileId: { type: String, label: 'Profile ID', dependsOn: deps.google },
-
       username: { type: String, label: 'Username', dependsOn: deps.google },
-      avatar: { type: String, label: 'Image', dependsOn: deps.google },
-
       accessToken: { type: String, label: 'Access Token', dependsOn: deps.google },
       refreshToken: { type: String, label: 'Refresh Token', dependsOn: deps.google }
     },
+    /**
+     * Twitter Service
+     * @typedef {Object} UserServices~Twitter
+     * @member {Boolean} isConfigured - `true` if configured
+     * @member {String} profileId - Twitter user profile ID
+     * @member {String} username - Twitter username
+     * @member {String} accessToken - OAuth access token
+     * @member {String} direct_messageSinceId - last DirectMessage
+    */
     twitter: {
       isConfigured: { type: Boolean, label: 'Twitter has been authenticated' },
-
       profileId: { type: String, label: 'Profile ID', dependsOn: deps.twitter },
-
       username: { type: String, label: 'Username', dependsOn: deps.twitter },
-      avatar: { type: String, label: 'Image', dependsOn: deps.twitter },
-
       accessToken: { type: String, label: 'Access Token', dependsOn: deps.twitter },
       refreshToken: { type: String, label: 'Refresh Token', dependsOn: deps.twitter }
     }
@@ -122,49 +109,12 @@ User.add({
 
 
 /**
-  Pre-save
-  =============
-*/
-
-User.schema.pre('save', function(next) {
-  var str = this.email.toLowerCase().trim()
-  this.gravatar = crypto.createHash('md5').update(str).digest('hex')
-  next()
-});
-
-
-/**
  * Virtuals
  * ========
  */
-
-// Link to member
-User.schema.virtual('url').get(function() {
-  return '/member/' + this.key
-})
-
-// Provide access to Keystone
 User.schema.virtual('canAccessKeystone').get(function() {
-  return this.isAdmin
-})
-
-// Pull out avatar image
-User.schema.virtual('avatarUrl').get(function() {
-  if (this.services.facebook.isConfigured && this.services.facebook.avatar) {
-    return this.services.facebook.avatar
-  }
-  if (this.services.google.isConfigured && this.services.google.avatar) {
-    return this.services.google.avatar
-  }
-  if (this.services.twitter.isConfigured && this.services.twitter.avatar) {
-    return this.services.twitter.avatar
-  }
-})
-
-// Usernames
-User.schema.virtual('twitterUsername').get(function() {
-  return (this.services.twitter && this.services.twitter.isConfigured) ? this.services.twitter.username : ''
-})
+  return this.isAdmin;
+});
 
 
 /**
@@ -172,24 +122,25 @@ User.schema.virtual('twitterUsername').get(function() {
  * =======
  */
 User.schema.methods.resetPassword = function(callback) {
-  this.resetPasswordKey = keystone.utils.randomString([16,24])
+  this.resetPasswordKey = keystone.utils.randomString([16,24]);
 
   var emailSendOpts = {
         user: this,
         link: '/reset-password/' + this.resetPasswordKey,
-        subject: 'Reset your '+keystone.get('brand')+' Password',
+        subject: 'Reset your ' + keystone.get('brand') + ' Password',
         to: this.email,
-        from: { name: keystone.get('brand'), email: keystone.get('brand email') }
-      }
+        from: {
+          name: keystone.get('brand'),
+          email: keystone.get('brand email')
+        }
+      };
 
   return this.save(function(err) {
-    if (err) {
-      callback(err)
-    } else {
-      (new keystone.Email('forgotten-password')).send(emailSendOpts, callback)
-    }
+    if (err) return callback(err);
+    var email = new keystone.Email('forgotten-password');
+    email.send(emailSendOpts, callback);
   });
-}
+};
 
 User.schema.methods.getKeywords = function() {
   if (this.notifications && this.notifications.keywords) {
@@ -198,9 +149,9 @@ User.schema.methods.getKeywords = function() {
       .uniq()
       .value();
   } else {
-    return []
+    return [];
   }
-}
+};
 
 //TODO paging
 User.schema.methods.getAlertDocuments = function(cb) {
@@ -240,9 +191,10 @@ User.schema.methods.getAlertDocuments = function(cb) {
       }
     }
   }, cb);
-}
+};
 
 User.schema.methods.sendNotificationEmail = function(links, callback) {
+  var self = this;
   //TODO set the subject and from values via keystone settings
   (new keystone.Email('notification')).send({
     subject: 'Cadence Notification',
@@ -254,53 +206,62 @@ User.schema.methods.sendNotificationEmail = function(links, callback) {
     links: links
   }, function(err, info) {
     if (err) {
-      console.error("Error sending notification email to %s", user.email);
+      console.error("Error sending notification email to %s", self.email);
     } else {
-      console.info("Sent notification email to %s", user.email);
+      console.info("Sent notification email to %s", self.email);
     }
 
     callback(err, info);
-  })
-  //
-}
+  });
+};
 
 User.schema.methods.facebookPages = function(callback) {
-  var self = this
+  var self = this;
   request({
-    url: 'https://graph.facebook.com/v2.3/me/accounts?access_token=' + user.services.facebook.accessToken,
+    url: 'https://graph.facebook.com/v2.3/me/accounts',
+    qs: {
+      access_token: this.services.facebook.accessToken
+    },
     json: true
   }, function (err, res, body) {
     if (err) {
-      console.error("Error getting pages for %s\n%s", user.id, body);
+      console.error("Error getting Facebook pages for %s\n%s", self.id, body);
       callback(err, body);
     } else {
       callback(null, body.data);
     }
-  })
-}
+  });
+};
 
 /**
  * Static Methods
  * ==============
  */
-User.schema.statics.findConnectedFacebook = function(cb) {
-  return this.find({ 'services.facebook.isConfigured': true }, cb);
-}
+User.schema.statics.findConnectedFacebook = function(callback) {
+  return this.find({ 'services.facebook.isConfigured': true }, callback);
+};
 
-User.schema.statics.findConnectedTwitter = function(cb) {
-  return this.find({ 'services.twitter.isConfigured': true }, cb);
-}
+User.schema.statics.findConnectedTwitter = function(callback) {
+  return this.find({ 'services.twitter.isConfigured': true }, callback);
+};
 
-User.schema.statics.findConnected = function(sources, cb) {
-  if (cb === undefined && _.isFunction(sources)) {
-    cb = sources;
+User.schema.statics.findConnectedGoogle = function(callback) {
+  return this.find({ 'services.google.isConfigured': true }, callback);
+};
+
+User.schema.statics.findConnected = function(sources, callback) {
+  if (callback === undefined && _.isFunction(sources)) {
+    callback = sources;
     sources = ["facebook", "twitter"];
   }
 
   if (!_.isArray(sources)) {
     sources = [sources];
   }
-  if (sources && !_.isArray(sources)) sources = [sources]
+
+  if (sources && !_.isArray(sources)) {
+    sources = [sources];
+  }
 
   return this.find({
     "$or": _.map(sources, function(s) {
@@ -308,23 +269,23 @@ User.schema.statics.findConnected = function(sources, cb) {
       result['services.' + s + '.isConfigured'] = true;
       return result;
     })
-  }, cb);
-}
+  }, callback);
+};
 // alias (deprecated)
-User.schema.statics.findConnectedUsers = User.schema.statics.findConnected
+User.schema.statics.findConnectedUsers = User.schema.statics.findConnected;
 
-User.schema.statics.findWithKeywords = function(cb) {
+User.schema.statics.findWithKeywords = function(callback) {
   return this.find({
     "notifications.keywords": {
       "$exists": true,
       "$nin": [ null, "" ]
     }
-  }, cb);
-}
+  }, callback);
+};
 
-User.schema.statics.findConnectedWithKeywords = function(sources, cb) {
-  if (cb === undefined && _.isFunction(sources)) {
-    cb = sources;
+User.schema.statics.findConnectedWithKeywords = function(sources, callback) {
+  if (callback === undefined && _.isFunction(sources)) {
+    callback = sources;
     sources = ["facebook", "twitter"];
   }
 
@@ -338,16 +299,16 @@ User.schema.statics.findConnectedWithKeywords = function(sources, cb) {
       result['services.' + s + '.isConfigured'] = true;
       return result;
     })
-  }, cb);
-}
+  }, callback);
+};
 
-User.schema.statics.findByID = function(id, cb) {
-  return this.find({id: id}, cb);
-}
+User.schema.statics.findByID = function(id, callback) {
+  return this.find({id: id}, callback);
+};
 
-User.schema.statics.findByEmail = function(email, cb) {
-  return this.find({email: email}, cb);
-}
+User.schema.statics.findByEmail = function(email, callback) {
+  return this.find({email: email}, callback);
+};
 
 /**
  * Registration
