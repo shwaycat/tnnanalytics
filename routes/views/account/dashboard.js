233cdc763f70e0ede4d46915bcbb092a863c0b3d
rnlambert7 <rnlambert7@gmail.com>
2015-05-06 15:07:11 -0400

    hid non used phase 1 pages and added redir to facebook

--- routes/views/account/dashboard.js
+++ routes/views/account/dashboard.js
@@ -2,6 +2,13 @@ var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
 
+	if (req.user) {
+		// Remove the facebook string once dashboard is back
+	  return res.redirect(req.cookies.target || '/accounts/'+req.user.accountName+'/facebook')
+	} else {
+		return res.redirect('/signin')
+	}
+
 	var view = new keystone.View(req, res),
 		locals = res.locals;
 
