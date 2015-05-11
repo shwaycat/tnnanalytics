ecff263559a3b169a24d161086e66a85b7dc646f
Travis Warlick <twarlick@maxmedia.com>
2015-05-11 01:34:57 -0400

    pull-facebook.js

--- scripts/pull-facebook.js
+++ scripts/pull-facebook.js
@@ -1,29 +1,42 @@
 require('dotenv').load();
 
-var keystone = require('../keystone-setup')(),
+var argv = require('minimist')(process.argv.slice(2)),
+    keystone = require('../keystone-setup')(),
     debug = require('debug')('cadence:pull'),
     User = keystone.list('User'),
     async = require('async'),
-    _ = require('underscore'),
     errorHandling = require('../lib/errorHandling'),
     connectES = require('../lib/connect_es'),
     facebookSource = require('../lib/sources/facebook');
 
-require('../lib/keystone-script')(connectES, function(done) {
-  async.eachSeries(_.keys(facebookSource), function(docTypeKey, nextDocType) {
-    console.info("Pulling from Facebook: %s", docTypeKey);
-
+function pullType(docType) {
+  return function(callback) {
     User.model.findAccountRoots(function(err, users) {
-      if (err) return nextDocType(err);
-
-      var docType = facebookSource[docTypeKey];
+      if (err) return callback(err);
 
       async.eachSeries(users, function(user, nextUser) {
         console.info("Pulling for user %s", user.id);
         debug("User: %j", user);
-        docType.pullAll(user, nextUser);
-      }, nextDocType);
+        if(argv.all) {
+          debug("Pull all");
+          docType.pullAll(user, nextUser);
+        } else {
+          debug("Pull latest");
+          docType.pull(user, nextUser);
+        }
+      }, callback);
     });
+  };
+}
+
+require('../lib/keystone-script')(connectES, function(done) {
+  async.auto({
+    pages: pullType(facebookSource.page),
+    messages: [ 'pages', pullType(facebookSource.message) ],
+    posts: [ 'pages', pullType(facebookSource.post) ],
+    statuses: [ 'pages', pullType(facebookSource.status) ],
+    comments: [ 'posts', 'statuses', pullType(facebookSource.comment) ],
+    mentions: [ 'comments', pullType(facebookSource.mention) ]
   }, function(err) {
     if (err) {
       errorHandling.logError(err);
