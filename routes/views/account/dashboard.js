94c369a0f60c5b55670ff8b74785278ee8d0381d
rnlambert7 <rnlambert7@gmail.com>
2015-05-19 16:17:48 -0400

    design adjustments

--- routes/views/account/dashboard.js
+++ routes/views/account/dashboard.js
@@ -2,10 +2,7 @@ var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
 
-  if (req.user) {
-    // Remove the facebook string once dashboard is back
-    return res.redirect(req.cookies.target || '/accounts/'+req.user.accountName+'/facebook')
-  } else {
+  if (!req.user) {
     return res.redirect('/signin')
   }
 
