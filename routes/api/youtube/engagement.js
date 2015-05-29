cfafc809e316225d2637f6ddec2ace4298f6e9b6
Jeremy Ciccone <jciccone@maxmedia.com>
2015-05-29 15:05:48 -0400

    refactor of existing youtube API endpoints

--- routes/api/youtube/engagement.js
+++ routes/api/youtube/engagement.js
@@ -1,21 +1,12 @@
 var keystone = require('keystone'),
+    moment = require('moment'),
+    debug = require('debug')('cadence:api:youtube:engagement'),
     _ = require('underscore'),
-    async = require('async'),
-    debug = require('debug')('cadence:api:instagram:engagement'),
-    User = keystone.list('User'),
-    mxm = require('../../../lib/mxm-utils');
+    youtubeMetrics = require('../../../lib/metrics/youtube');
 
-
-
-exports = module.exports = function(req, res) {
-
-  var view = new keystone.View(req, res),
-      locals = res.locals,
-      dataReturn = [];
-
-  startTime = new Date();
-  endTime = new Date();
-  startTime.setDate(endTime.getDate() - 30);
+module.exports = function(req, res) {
+  var startTime = moment().subtract(1, 'month').toDate(),
+      endTime = new Date();
 
   if(req.query.startTime) {
     startTime = new Date(req.query.startTime);
@@ -24,172 +15,19 @@ exports = module.exports = function(req, res) {
     endTime = new Date(req.query.endTime);
   }
 
-  var dataReturn = [];
-  var timeHolder = startTime;
+  youtubeMetrics.engagement(req.user, startTime, endTime, function(response) {
+    debug(response);
 
-  var interval = (endTime.getTime() - startTime.getTime()) / 24;
-      interval = interval / 1000;
+    if(response.error) {
+      return res.apiResponse(response);  
+    } else {
+      response.success = true;
+      response.type = 'engagement';
+      response.source = 'youtube';
+      response.queryString = req.queryString;
+      return res.apiResponse(response);
+    }
+    
+  }); 
 
-  User.model.getAccountRootInfo(req.user.accountName, function(err, accountRoot) {
-    if (err) return apiResponse({'error': err});
-
-    keystone.elasticsearch.search({
-      index: keystone.get('elasticsearch index'),
-      type: "youtube",
-      body: {
-        "query": {
-          "filtered": {
-            "filter": {
-              "and": {
-                "filters": [
-                  {
-                    "or": {
-                      "filters": [
-                        {
-                          "term": {
-                            "doc_type": "video"
-                          }
-                        }
-                      ]
-                    }
-                  },
-                  {
-                    "and": {
-                      "filters": [
-                        {
-                          "term": {
-                            "cadence_user_id": accountRoot.id
-                          }
-                        }
-                      ]
-                    }
-                  },
-                  {
-                    "range": {
-                      "timestamp": {
-                        "gte": startTime,
-                        "lte": endTime
-                      }
-                    }
-                  }
-                ]
-              }
-            }
-          }
-        },
-        "aggs": {
-          "engagement": {
-            "date_histogram": {
-              "field": "timestamp",
-              "interval": "3600s",
-              "min_doc_count": 0
-            },
-            "aggs": {
-              "commentCount": {
-                "max": {
-                  "field": "commentCount"
-                }
-              },
-              "viewCount": {
-                "max": {
-                  "field": "viewCount"
-                }
-              },
-              "likeCount": {
-                "max": {
-                  "field": "likeCount"
-                }
-              },
-              "shareCount": {
-                "max": {
-                  "field": "shareCount"
-                }
-              },
-              "dislikeCount": {
-                "max": {
-                  "field": "dislikeCount"
-                }
-              }              
-            }
-          }
-        }
-      }
-    }, function(err, response) {
-      if(err) return res.apiResponse({"error": err});
-      var dataReturn = [],
-          buckets = mxm.objTry(response, 'aggregations', 'engagement', 'buckets');
-
-      if(buckets && buckets.length) {
-
-        if(buckets.length == 1) {
-          first = {
-            key:startTime.toISOString(),
-            viewCount: 0,
-            likeCount: 0,
-            dislikeCount: 0,
-            commentCount: 0,
-            shareCount: 0,
-            value: 0
-          }
-          dataReturn.push(first);
-        }
-
-        _.each(buckets, function(bucket){
-          dataReturn.push(extractDataPoint(bucket));
-        });
-
-        if(buckets.length == 1) {
-          last = {
-            key:endTime.toISOString(),
-            viewCount: 0,
-            likeCount: 0,
-            dislikeCount: 0,
-            commentCount: 0,
-            shareCount: 0,
-            value: 0
-          }
-          dataReturn.push(last);
-        }
-
-        return res.apiResponse({
-          success: true,
-          type: 'engagement',
-          source: 'youtube',
-          queryString: req.query,
-          data: dataReturn,
-          summary: {
-            "totalViews" : _.reduce(dataReturn, function(memo, dataPoint) { return memo + dataPoint.viewCount; }, 0),
-            "totalLikes" : _.reduce(dataReturn, function(memo, dataPoint) { return memo + dataPoint.likeCount; }, 0),
-            "totalDislikes" : _.reduce(dataReturn, function(memo, dataPoint) { return memo + dataPoint.dislikeCount; }, 0),
-            "totalComments" : _.reduce(dataReturn, function(memo, dataPoint) { return memo + dataPoint.commentCount; }, 0),
-            "totalShares" : _.reduce(dataReturn, function(memo, dataPoint) { return memo + dataPoint.shareCount; }, 0)
-          }
-        });
-      } else {
-        res.apiResponse('error', "No buckets.")
-      }
-    });
-  });
-}
-
-function extractDataPoint(bucket) {
-  var key = bucket.key_as_string,
-      viewCount = (bucket.viewCount.value || 0),
-      likeCount = (bucket.likeCount.value || 0),
-      dislikeCount = (bucket.dislikeCount.value || 0),
-      commentCount = (bucket.commentCount.value || 0),
-      shareCount = (bucket.shareCount.value || 0),
-      value;
-
-  value = viewCount + likeCount - dislikeCount + commentCount + shareCount;
-
-  return {
-      key: key,
-      viewCount: viewCount,
-      likeCount: likeCount,
-      dislikeCount: dislikeCount,
-      commentCount: commentCount,
-      shareCount: shareCount,
-      value: value
-  };
-}
+};
--- routes/api/youtube/topVideo.js
+++ routes/api/youtube/topVideo.js
@@ -1,22 +1,12 @@
 var keystone = require('keystone'),
+    moment = require('moment'),
+    debug = require('debug')('cadence:api:youtube:topVideo'),
     _ = require('underscore'),
-    async = require('async'),
-    debug = require('debug')("cadence:api:youtube:topVideo"),
-    request = require('request'),
-    mxm = require('../../../lib/mxm-utils'),
-    User = keystone.list('User'),
-    Video = require('../../../lib/sources/youtube/video');
+    youtubeMetrics = require('../../../lib/metrics/youtube');
 
-
-exports = module.exports = function(req, res) {
- 
-  var view = new keystone.View(req, res),
-      locals = res.locals;
-  
-  var dataReturn = [];
-  endTime = new Date();
-  startTime = new Date();
-  startTime.setDate(endTime.getDate() - 90);
+module.exports = function(req, res) {
+  var startTime = moment().subtract(1, 'month').toDate(),
+      endTime = new Date();
 
   if(req.query.startTime) {
     startTime = new Date(req.query.startTime);
@@ -25,96 +15,19 @@ exports = module.exports = function(req, res) {
     endTime = new Date(req.query.endTime);
   }
 
+  youtubeMetrics.topVideo(req.user, startTime, endTime, function(response) {
+    debug(response);
 
-  // Build Response Here
-  User.model.getAccountRootInfo(req.user.accountName, function(err, accountRoot) {
-    if (err) return apiResponse({'error': err});
+    if(response.error) {
+      return res.apiResponse(response);  
+    } else {
+      response.success = true;
+      response.type = 'topVideo';
+      response.source = 'youtube';
+      response.queryString = req.queryString;
+      return res.apiResponse(response);
+    }
+    
+  }); 
 
-    keystone.elasticsearch.search({
-      index: keystone.get('elasticsearch index'),
-      from: 0,
-      size: 1000000000,
-      body: {
-        "query": {
-          "filtered": {
-            "query": {
-              "term": {
-                "doc_type": {
-                  "value": "video"
-                }
-              }
-            },
-            "filter": {
-              "and": {
-                "filters": [
-                  {
-                    "range": {
-                      "timestamp": {
-                        "gte": startTime,
-                        "lte": endTime
-                      }
-                    }
-                  },
-                  {
-                    "term": {
-                      "cadence_user_id": accountRoot.id
-                    }
-                  }
-                ]
-              }
-            }
-          }
-        }
-      }
-    }, function(err, response) {
-      if(err) return res.apiResponse({"error": err});
-
-      var videos = mxm.objTry(response, 'hits', 'hits');
-          scoredVideos = [];
-        
-        if(!videos || videos.length == 0) return res.apiResponse({"error": "Error with ES search results."});
-
-        async.eachSeries(videos, function(video, nextVideo) {
-          Video.findOne(video._id, function(err, video) {
-            if(err) return nextVideo(err);
-
-            video.modifyByDelta(function(err) {
-              if (err) return nextVideo(err);
-
-              scoredVideos.push({
-                id: video.id,
-                url: video.url,
-                viewCount: video.viewCount || 0,
-                likeCount: video.likeCount || 0,
-                commentCount: video.commentCount || 0,
-                shareCount: video.shareCount || 0,
-                score: (parseInt(video.viewCount) || 0) + (parseInt(video.likeCount) || 0) + (parseInt(video.commentCount) || 0) + (parseInt(video.shareCount) || 0)
-              });
-              nextVideo();
-            });
-          });
-        }, function(err) {
-          if(err) return res.apiResponse({"error": err});
-
-          var max = {};
-          if(!scoredVideos || scoredVideos.length == 0) return res.apiResponse({"error": 'Error in async?'});
-
-          max = _.max(scoredVideos, function(video) { return video.score; });
-
-          if(!max) return res.apiResponse({"error": "Something went way wrong."})
-
-          dataReturn = {
-            success: true,
-            type: 'topPost',
-            source: 'youtube',
-            queryString: req.query,
-            data: max
-          }
-          return res.apiResponse(dataReturn);
-
-
-
-      });
-    });
-  });
-}
+};
