4e2c50d94ce630b97ef23295328b02f1ee026177
Jeremy Ciccone <jciccone@maxmedia.com>
2015-05-29 15:16:55 -0400

    refactor of G+ api

--- routes/api/googleplus/acquisition.js
+++ routes/api/googleplus/acquisition.js
@@ -1,19 +1,12 @@
 var keystone = require('keystone'),
+    moment = require('moment'),
+    debug = require('debug')('cadence:api:googleplus:acquisition'),
     _ = require('underscore'),
-    async = require('async'),
-    User = keystone.list('User'),
-    mxm = require('../../../lib/mxm-utils');
+    googleplusMetrics = require('../../../lib/metrics/googleplus');
 
-
-exports = module.exports = function(req, res) {
- 
-  var view = new keystone.View(req, res),
-      locals = res.locals;
-
-  var dataReturn = [];
-  endTime = new Date();
-  startTime = new Date();
-  startTime.setDate(endTime.getDate() - 30);
+module.exports = function(req, res) {
+  var startTime = moment().subtract(1, 'month').toDate(),
+      endTime = new Date();
 
   if(req.query.startTime) {
     startTime = new Date(req.query.startTime);
@@ -22,99 +15,19 @@ exports = module.exports = function(req, res) {
     endTime = new Date(req.query.endTime);
   }
 
-  var interval = (endTime.getTime() - startTime.getTime()) / 24;
-      interval = interval / 1000;
+  googleplusMetrics.acquisition(req.user, startTime, endTime, function(response) {
+    debug(response);
 
-  User.model.getAccountRootInfo(req.user.accountName, function(err, accountRoot) {
-    if (err) return apiResponse({'error': err});
+    if(response.error) {
+      return res.apiResponse(response);  
+    } else {
+      response.success = true;
+      response.type = 'acquisition';
+      response.source = 'googleplus';
+      response.queryString = req.queryString;
+      return res.apiResponse(response);
+    }
+    
+  }); 
 
-    keystone.elasticsearch.search({
-      index: keystone.get('elasticsearch index'),
-      body: {
-        "query": {
-          "filtered": {
-            "filter": {
-              "and": {
-                "filters": [
-                  { "exists": { "field": "circledByCount" } },
-                  { "range": {
-                      "timestamp": {
-                        "gte": startTime,
-                        "lte": endTime
-                      }
-                    } 
-                  },
-                  {
-                    "term": {
-                      "cadence_user_id": accountRoot.id
-                    }
-                  }
-                ]
-              }
-            }
-          }
-        },
-        "aggs": {
-          "followers": {
-            "date_histogram": {
-              "field": "timestamp",
-              "interval": interval + "s",
-              "min_doc_count": 0
-            },
-            "aggs": {
-              "avg_follower_count": {
-                "avg": {
-                  "field": "circledByCount"
-                }
-              }
-            }
-          }
-        }
-      }
-    }, function(err, response) {
-      if(err) return res.apiResponse({"error": err});
-      var dataReturn = [],
-          buckets = mxm.objTry(response, 'aggregations', 'followers', 'buckets');
-
-      if(buckets && buckets.length) {
-        dataReturn.push({
-          key: startTime.toISOString(),
-          value: buckets[0].avg_follower_count.value
-        });
-
-        _.each(buckets, function(bucket){
-          if(bucket.avg_follower_count && bucket.avg_follower_count.value) {
-            dataReturn.push({
-              key: bucket.key_as_string,
-              value: bucket.avg_follower_count.value
-            })
-          }
-        });
-
-        dataReturn.push({
-          key: endTime.toISOString(),
-          value: _.last(dataReturn).value
-        });
-
-
-        return res.apiResponse({
-          success: true,
-          type: 'acquisition',
-          source: 'googleplus',
-          queryString: req.query,
-          data: dataReturn,
-          summary: {
-            "totalCircledBy": _.last(dataReturn).value,
-            "changeInCircledBy": _.last(dataReturn).value - _.first(dataReturn).value
-          }
-        });    
-      } else {
-        res.apiResponse({'error': "No Buckets."})
-      }
-    });
-  });
-  
-
-
-
-}
+};
--- routes/api/googleplus/engagement.js
+++ routes/api/googleplus/engagement.js
@@ -1,21 +1,12 @@
 var keystone = require('keystone'),
-    _ = require('underscore'),
-    async = require('async'),
+    moment = require('moment'),
     debug = require('debug')('cadence:api:googleplus:engagement'),
-    User = keystone.list('User'),
-    mxm = require('../../../lib/mxm-utils');
+    _ = require('underscore'),
+    googleplusMetrics = require('../../../lib/metrics/googleplus');
 
-
-
-exports = module.exports = function(req, res) {
-
-  var view = new keystone.View(req, res),
-      locals = res.locals,
-      dataReturn = [];
-
-  startTime = new Date();
-  endTime = new Date();
-  startTime.setDate(endTime.getDate() - 30);
+module.exports = function(req, res) {
+  var startTime = moment().subtract(1, 'month').toDate(),
+      endTime = new Date();
 
   if(req.query.startTime) {
     startTime = new Date(req.query.startTime);
@@ -24,167 +15,19 @@ exports = module.exports = function(req, res) {
     endTime = new Date(req.query.endTime);
   }
 
-  var dataReturn = [];
-  var timeHolder = startTime;
+  googleplusMetrics.engagement(req.user, startTime, endTime, function(response) {
+    debug(response);
 
-  var interval = (endTime.getTime() - startTime.getTime()) / 24;
-      interval = interval / 1000;
+    if(response.error) {
+      return res.apiResponse(response);  
+    } else {
+      response.success = true;
+      response.type = 'engagement';
+      response.source = 'googleplus';
+      response.queryString = req.queryString;
+      return res.apiResponse(response);
+    }
+    
+  }); 
 
-  User.model.getAccountRootInfo(req.user.accountName, function(err, accountRoot) {
-    if (err) return apiResponse({'error': err});
-
-    keystone.elasticsearch.search({
-      index: keystone.get('elasticsearch index'),
-      type: "googleplus",
-      body: {
-        "query": {
-          "filtered": {
-            "filter": {
-              "and": {
-                "filters": [
-                  {
-                    "or": {
-                      "filters": [
-                        {
-                          "term": {
-                            "doc_type": "post"
-                          }
-                        },
-                        {
-                          "exists": {
-                            "field": "replies"
-                          }
-                        },
-                        {
-                          "exists": {
-                            "field": "plusoners"
-                          }
-                        },
-                        {
-                          "exists": {
-                            "field": "resharers"
-                          }
-                        }
-                      ]
-                    }
-                  },
-                  {
-                    "and": {
-                      "filters": [
-                        {
-                          "term": {
-                            "cadence_user_id": accountRoot.id
-                          }
-                        }
-                      ]
-                    }
-                  },
-                  {
-                    "range": {
-                      "timestamp": {
-                        "gte": startTime,
-                        "lte": endTime
-                      }
-                    }
-                  }
-                ]
-              }
-            }
-          }
-        },
-        "aggs": {
-          "engagement": {
-            "date_histogram": {
-              "field": "timestamp",
-              "interval": "3600s",
-              "min_doc_count": 0
-            },
-            "aggs": {
-              "comments": {
-                "max": {
-                  "field": "replies"
-                }
-              },
-              "plusoners": {
-                "max": {
-                  "field": "plusoners"
-                }
-              },
-              "resharers": {
-                "max": {
-                  "field": "resharers"
-                }
-              }
-            }
-          }
-        }
-      }
-    }, function(err, response) {
-      if(err) return res.apiResponse({"error": err});
-      var dataReturn = [],
-          buckets = mxm.objTry(response, 'aggregations', 'engagement', 'buckets');
-
-      if(buckets && buckets.length) {
-
-        if(buckets.length == 1) {
-          first = {
-            key:startTime.toISOString(),
-            plusoners:0,
-            comments:0,
-            resharers:0,
-            value:0
-          }
-          dataReturn.push(first);
-        }
-
-        _.each(buckets, function(bucket){
-          dataReturn.push(extractDataPoint(bucket));
-        });
-
-        if(buckets.length == 1) {
-          last = {
-            key:endTime.toISOString(),
-            plusoners:0,
-            comments:0,
-            resharers:0,
-            value:0
-          }
-          dataReturn.push(last);
-        }
-
-        return res.apiResponse({
-          success: true,
-          type: 'engagement',
-          source: 'googleplus',
-          queryString: req.query,
-          data: dataReturn,
-          summary: {
-            "totalPlusOners" : _.reduce(dataReturn, function(memo, dataPoint) { return memo + dataPoint.plusoners; }, 0),
-            "totalResharers" : _.reduce(dataReturn, function(memo, dataPoint) { return memo + dataPoint.resharers; }, 0),
-            "totalComments" : _.reduce(dataReturn, function(memo, dataPoint) { return memo + dataPoint.comments; }, 0)
-          }
-        });
-      } else {
-        res.apiResponse('error', "No buckets.")
-      }
-    });
-  });
-}
-
-function extractDataPoint(bucket) {
-  var key = bucket.key_as_string,
-      plusoners = (bucket.plusoners.value || 0),
-      comments = (bucket.comments.value || 0),
-      resharers = (bucket.resharers.value || 0),
-      value;
-
-  value = plusoners + comments + resharers;
-
-  return {
-      key: key,
-      plusoners: plusoners,
-      comments: comments,
-      resharers: resharers,
-      value: value
-  };
-}
+};
